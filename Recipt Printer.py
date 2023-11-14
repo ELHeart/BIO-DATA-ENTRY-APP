ddbc9d61e23145e47136af9407511efea9bd7718
Davidson Kwaipe Boye <117992838+ELHeart@users.noreply.github.com>
2023-11-14 09:59:52 +0000

    Update Recipt Printer.py
    Changed user consent authentication from While loop to if Conditional statement

--- Recipt Printer.py
+++ Recipt Printer.py
@@ -6,10 +6,6 @@ print("Kindly follow the instructions ad contact the System administrator if you
 print("Enter 1 to begin bio-data entry or 0 to cancel.")
 entry = int(input())
 # Consent entry correction point and re-entry
-while entry != 1 or 0:
-    print("Enter either 1 to begin or 0 to cancel")
-    entry = int(input())
-
 # Bio-data Input begins
 if entry == 1:
     print("Bio-data entry begins")
@@ -24,3 +20,6 @@ if entry == 1:
 elif entry == 0:
     print("Bio-data entry was cancelled by the user!")
     print("Thank You")
+else
+    print(" Invalid input!!! Enter 1 or 0")
+    entry = int(input())
