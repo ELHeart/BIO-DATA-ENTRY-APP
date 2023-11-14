e2bc6b02b4779397a43c9a91011a407d17d4612a
Davidson Kwaipe Boye <tobysonevangel@gmail.com>
2023-11-14 12:27:28 +0000

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Recipt Printer.py

--- Recipt Printer.py
+++ Recipt Printer.py
@@ -1,13 +1,28 @@
-# Introduction to the software
-print("Hello and welcome to the Bio-data Entry App.")
+# BIO-CAP is a python based Bio-data collection application currently under development.
+# in this repository you will find detailed information about this application in the README file attached.
+# Kindly assist with any errors or bugs you may find in the program.
+print(" ")
+print(" ")
+# Introduction to the software.
+print("***********************************************************")
+print("Hello and Welcome to BIO-CAP. A Bio-data Entry Application.")
 print("Kindly follow the instructions ad contact the System administrator if you need help")
-
+print("***********************************************************")
+print(" ")
 # User consent
+print("***********************************************************")
 print("Enter 1 to begin bio-data entry or 0 to cancel.")
 entry = int(input())
+
 # Consent entry correction point and re-entry
-# Bio-data Input begins
+while entry > 1:
+    print(" Invalid input!!! Enter 1 or 0")
+    entry = int(input())
+print(" ")
+print("***********************************************************")
+# Bio-data Input begins if consent is given.
 if entry == 1:
+    print(" ")
     print("Bio-data entry begins")
     print("*******************************************")
     print("Enter your Name: ")
@@ -20,6 +35,3 @@ if entry == 1:
 elif entry == 0:
     print("Bio-data entry was cancelled by the user!")
     print("Thank You")
-else:
-    print(" Invalid input!!! Enter 1 or 0")
-    entry = int(input())
