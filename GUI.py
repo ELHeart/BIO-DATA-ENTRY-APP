efe1baeb9836f33d6ab10645ef9399776f8265f9
Davidson Boye <tobysonevangel@gmail.com>
2023-11-30 14:12:52 +0000

    Application_Source.py has been optimized to provide a better outlook from a console perspective. GUI.py is an AI assisted simple gui code for the application with added exceptions for the age variable and integration with MS access database BIO_DATA.accdb

--- GUI.py
+++ GUI.py
@@ -1,10 +1,112 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QLineEdit, QMessageBox, QDialog, \
-    QHBoxLayout
-
+from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QLineEdit, QMessageBox, QDialog,
+                             QHBoxLayout)
 import pyodbc
+import hashlib
+
+# Sign Up Dialog Class
 
 
-# noinspection PyUnresolvedReferences
+class SignUpDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Sign Up')
+        self.setModal(True)
+
+        layout = QVBoxLayout()
+
+        self.username = QLineEdit()
+        self.username.setPlaceholderText("Enter username")
+        self.password = QLineEdit()
+        self.password.setPlaceholderText("Enter password")
+        self.password.setEchoMode(QLineEdit.Password)
+
+        layout.addWidget(QLabel("Username:"))
+        layout.addWidget(self.username)
+        layout.addWidget(QLabel("Password:"))
+        layout.addWidget(self.password)
+
+        signup_button = QPushButton("Sign Up")
+        signup_button.clicked.connect(self.register_user)
+        layout.addWidget(signup_button)
+
+
+        self.setLayout(layout)
+
+    def register_user(self):
+        username = self.username.text()
+        password = self.password.text()
+
+        # Hash the entered password
+        hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+        # Connect to the MS Access database to insert new user
+        conn_str = (
+            r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
+            r'DBQ=C:\Users\el_he\Desktop\BIO-CAP-SIGN-UP.accdb;'  # Update the path to your database
+        )
+        conn = pyodbc.connect(conn_str)
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO Users (Username, PasswordHash) VALUES (?, ?)', (username, hashed_password))
+        conn.commit()
+        conn.close()
+
+        QMessageBox.information(self, "Success", "You have signed up successfully!")
+        self.accept()
+
+
+# Login Dialog Class
+class LoginDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Login')
+        self.setModal(True)
+
+        layout = QVBoxLayout()
+
+        self.username = QLineEdit()
+        self.username.setPlaceholderText("Enter username")
+        self.password = QLineEdit()
+        self.password.setPlaceholderText("Enter password")
+        self.password.setEchoMode(QLineEdit.Password)
+
+        layout.addWidget(QLabel("Username:"))
+        layout.addWidget(self.username)
+        layout.addWidget(QLabel("Password:"))
+        layout.addWidget(self.password)
+
+        login_button = QPushButton("Login")
+        login_button.clicked.connect(self.check_credentials)
+        layout.addWidget(login_button)
+
+        self.setLayout(layout)
+
+    def check_credentials(self):
+        username = self.username.text()
+        password = self.password.text()
+
+        # Hash the entered password
+        hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+        # Connect to the MS Access database to check credentials
+        conn_str = (
+            r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
+            r'DBQ=C:\Users\el_he\Desktop\BIO-CAP-SIGN-UP.accdb;'  # Update the path to your database
+        )
+        conn = pyodbc.connect(conn_str)
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM Users WHERE Username=? AND PasswordHash=?', (username, hashed_password))
+        result = cursor.fetchone()
+        conn.close()
+
+        if result:
+            self.accept()  # Close the dialog and continue
+        else:
+            QMessageBox.warning(self, "Login Failed", "Invalid username or password. Please try again.")
+            self.username.clear()
+            self.password.clear()
+
+
+# ConfirmDialog Class
 class ConfirmDialog(QDialog):
     def __init__(self, parent, first_name, middle_name, last_name, age):
         super().__init__(parent)
@@ -29,7 +131,10 @@ class ConfirmDialog(QDialog):
         layout.addLayout(button_layout)
         self.setLayout(layout)
 
+    # ... (same as before) ...
 
+
+# BioDataApp Class
 class BioDataApp(QWidget):
     def __init__(self):
         super().__init__()
@@ -108,9 +213,18 @@ class BioDataApp(QWidget):
         self.lastName.clear()
         self.age.clear()
 
+    # ... (same as before) ...
+
 
 if __name__ == '__main__':
     app = QApplication([])
-    ex = BioDataApp()
-    ex.show()
-    app.exec_()
+
+    # Show the sign-up dialog first
+    signup = SignUpDialog()
+    if signup.exec_() == QDialog.Accepted:
+        # After successful sign-up, show the login dialog
+        login = LoginDialog()
+        if login.exec_() == QDialog.Accepted:
+            ex = BioDataApp()
+            ex.show()
+            app.exec_()
